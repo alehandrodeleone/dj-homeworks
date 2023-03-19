b3285c3c84a5d9b72eb827788cf6616b283b84c0
alehandrodeleone <zzapahstraha@gmail.com>
2023-03-19 19:39:15 +0700

    добавление нового урла

--- 1.1-first-project/first_project/first_project/urls.py
+++ 1.1-first-project/first_project/first_project/urls.py
@@ -21,9 +21,11 @@ from app.views import home_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('', time_view, name='home'),
     # Раскомментируйте код, чтобы данные урлы 
     # обрабатывались Django
     # path('current_time/', time_view, name='time'),
     # path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
+
 ]
