7041fdccc3234a9ecd669130a32bc3a229635dd8
alehandrodeleone <zzapahstraha@gmail.com>
2023-04-22 21:45:13 +0700

    complete

--- 2.2-databases-2/orm_migrations/school/urls.py
+++ 2.2-databases-2/orm_migrations/school/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from school.views import students_list
+from school.views import students_view
 
 urlpatterns = [
-    path('', students_list, name='students'),
+    path('', students_view, name='students'),
 ]
--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -4,12 +4,10 @@ from django.shortcuts import render
 from .models import Student
 
 
-def students_list(request):
+def students_view(request):
     template = 'school/students_list.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = 'group'
+    students = Student.objects.all()
+    context = {'students': students}
 
     return render(request, template, context)
+# .prefetch_related('teachers').order_by('group')
